776b66d5142e6964277f6cf0f65ea08e9206706a
Onur Omer SAHIN <onuromersahin@gmail.com>
2022-12-06 21:52:13 +0300

    created post detail page

---
+++ src/Controller/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/posts", name="app_post")
+     */
+    public function list(): Response
+    {
+        return $this->render('post/list.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    /**
+     * @Route("/post/detail", name="app_post")
+     */
+    public function view(): Response
+    {
+        return $this->render('post/detail.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+}
