c7f33f36f29b765900a91d7efc7d25d5c3aa3a5c
Onur Omer SAHIN <onuromersahin@gmail.com>
2022-12-25 13:55:17 +0300

    created add and list method for tags and categories

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/admin/categories", name="app_admin_category")
+     * @Route("/admin/categories", name="admin_categories")
      */
-    public function list(): Response
+    public function list(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('admin/category/list.html.twig', [
             'controller_name' => 'CategoryController',
+            'categories'=>$categories
         ]);
     }
 
@@ -22,8 +29,16 @@ class CategoryController extends AbstractController
     /**
      * @Route("/admin/category/add", name="app_admin_category_add")
      */
-    public function addPost(): Response
+    public function addCategory(Request $request,EntityManagerInterface $entityManager): Response
     {
+        if ($request->getMethod()===Request::METHOD_POST){
+            $category = new Category();
+            $category->setTitle($request->request->get('title'));
+            $category->setCreatedAt(new \DateTime());
+            $entityManager->persist($category);
+            $entityManager->flush($category);
+            return $this->redirectToRoute('admin_categories');
+        }
         return $this->render('admin/category/add.html.twig', [
             'controller_name' => 'PostController',
         ]);
--- src/Controller/Admin/TagController.php
+++ src/Controller/Admin/TagController.php
@@ -2,27 +2,42 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Tag;
+use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TagController extends AbstractController
 {
     /**
-     * @Route("/admin/tags", name="app_admin_tag")
+     * @Route("/admin/tags", name="admin_tags")
      */
-    public function index(): Response
+    public function list(TagRepository $tagRepository): Response
     {
+        $tags = $tagRepository->findAll();
+
         return $this->render('admin/tag/list.html.twig', [
             'controller_name' => 'TagController',
+            'tags'=>$tags
         ]);
     }
 
     /**
      * @Route("/admin/tag/add", name="app_admin_tag_add")
      */
-    public function addPost(): Response
+    public function addTag(Request $request, EntityManagerInterface $entityManager)
     {
+        if ($request->getMethod()===Request::METHOD_POST){
+            $tag = new Tag();
+            $tag->setTitle($request->request->get('title'));
+            $tag->setCreatedAt(new \DateTime());
+            $entityManager->persist($tag);
+            $entityManager->flush($tag);
+            return $this->redirectToRoute("admin_tags");
+        }
         return $this->render('admin/tag/add.html.twig', [
             'controller_name' => 'PostController',
         ]);
